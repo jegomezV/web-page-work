2677c127e2a0f09a86f741e8840194145191a166
jegomezV <jegomezvalencia@gmail.com>
2024-06-04 19:29:47 -0500

    Config Bg

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,8 +3,49 @@ import './App.css'
 function App() {
 
   return (
-    <div className=''>
+    <div className='min-h-screen flex flex-col justify-center items-center '>
       <h1 className='text-white font-bold'>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
+      <h1>Hola</h1>
       <h1>Hola</h1>
       <h1>Hola</h1>
       <h1>Hola</h1>
--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Bg } from './components/Bg.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Bg>
-        <App />
-      </Bg>
-    
+    <div className="absolute overflow-x-hidden top-0 z-[-2] w-screen bg-color-bg1 bg-[radial-gradient(ellipse_50%_90%_at_50%_-20%,rgba(1,2,200,0.3),rgba(255,255,255,0))]">
+      <App />
+    </div>
   </React.StrictMode>,
 )
