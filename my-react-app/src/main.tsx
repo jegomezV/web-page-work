8b5e6c94be6f27c199470baf7e374c21fca49474
Luamix <luizalfonso611@gmail.com>
2024-06-04 20:08:48 -0500

    Change rgba color

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="absolute overflow-x-hidden top-0 z-[-2] w-screen bg-color-bg1 bg-[radial-gradient(ellipse_50%_90%_at_50%_-20%,rgba(1,2,200,0.3),rgba(255,255,255,0))]">
+    <div className="absolute overflow-x-hidden top-0 z-[-2] w-screen bg-color-bg1 bg-[radial-gradient(ellipse_100%_300%_at_50%_-20%,rgba(193,201,217,0.3),rgba(255,255,255,0))]">
       <App />
     </div>
   </React.StrictMode>,
