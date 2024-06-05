5ae1990f6bcf8e49db08d49512fd31b025cce01d
jegomezV <jegomezvalencia@gmail.com>
2024-06-04 20:54:30 -0500

    bg-white finished

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,59 +3,40 @@ import './App.css'
 function App() {
 
   return (
-    <div className='min-h-screen flex flex-col justify-center items-center '>
-      <h1 className='text-white font-bold'>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
-      <h1>Hola</h1>
+    <div className='min-h-screen flex flex-col justify-center items-center'>
+      <h1 className='text-black font-bold'>Hola</h1>
+      <h1 className='text-black font-bold'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
+      <h1 className='text-black'>Hola</h1>
     </div>
   )
 }
--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -5,8 +5,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="absolute overflow-x-hidden top-0 z-[-2] w-screen bg-color-bg1 bg-[radial-gradient(ellipse_50%_90%_at_50%_-20%,rgba(193,201,250,0.3),rgba(255,255,255,0))]">
+   <div className="absolute overflow-x-hidden top-0 z-[-2] w-screen bg-color-bg1 bg-[radial-gradient(ellipse_50%_90%_at_50%_-20%,rgba(193,201,250,0.3),rgba(255,255,255,0))]">
       <App />
     </div>
+    {/* <div className="relative bg-slate-200 -z-10 h-full w-screen border-2 border-white">
+      <div className="fixed -z-10 h-full w-screen bg-[radial-gradient(#9d9d9d_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+      <App />
+    </div> */}
   </React.StrictMode>,
 )
