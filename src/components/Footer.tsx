89b57d182372d71f9254936d9a5834954ba74c6a
jegomezV <jegomezvalencia@gmail.com>
2024-06-10 04:38:59 -0500

    Adds first pics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer: React.FC = (): JSX.Element => {
   return (
     <footer className=''>
-      <h1 className='bottom-0 p-4 border-[1px] text-5xl text-center bg-black rounded-t-xl'>Footer</h1>
+      <h1 className='fixed w-full mx-auto bottom-0 p-4 border-[1px] bg-transparent text-5xl text-center bg-black rounded-t-xl'>Footer</h1>
     </footer>
   );
 };
